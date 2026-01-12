caa9f36097266d89fb1ccc8924d76e6c2b476a8a
Ismael Ors <ismikin@gmail.com>
2026-01-12 15:07:00 +0100

    feat(all): Init first view, comps and router

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Sidebar from '@/components/SideBar.vue'
+</script>
+
+<template>
+  <div>
+    <nav>
+      <Sidebar />
+    </nav>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      redirect: 'list-products',
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/About.vue'),
+    },
+    {
+      path: '/products',
+      name: 'list-products',
+      component: () => import('@/products/views/ListProducts.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: () => ({ name: 'list-products' }),
+    },
+  ],
 })
 
 export default router
